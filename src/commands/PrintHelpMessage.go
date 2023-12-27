6a2b357c963353067509c4ee5ee99877df3845cf
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2023-12-27 18:24:40 -0500

    docs: add to command funcs

--- src/commands/PrintHelpMessage.go
+++ src/commands/PrintHelpMessage.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+// PrintHelpMessage prints the help message for all commands.
+// It iterates over the list of commands obtained from GetCommands()
+// and calls printCommandHelp() to print the help message for each command.
+// It returns an error if there is any issue while printing the help message.
 func PrintHelpMessage() error {
 	for _, command := range *GetCommands() {
 		printCommandHelp(&command)
