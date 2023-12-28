b8563d81d73854b5e7846a65c92f92ef0c18b662
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2023-12-27 21:01:16 -0500

    feat: bootstrap repl and commands

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import (
+	"github.com/KerickHowlett/pokedexcli/src/commands"
+	"github.com/KerickHowlett/pokedexcli/src/repl"
+)
 
+func main() {
+	repl.RunREPL(commands.RunCommand)
 }
