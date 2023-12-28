6a6b5e50ebab8439b83470c0e231435f629db905
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2023-12-27 19:06:46 -0500

    refactor: create commands type

--- src/commands/getcommands.go
+++ src/commands/getcommands.go
@@ -1,11 +1,13 @@
 package commands
 
+type Commands = map[string]Command
+
 // GetCommands returns a map of commands available in the CLI.
 // Each command is represented by a key-value pair, where the key is the command
 // name and the value is a Command struct containing the command's name,
 // description, and execute function.
-func GetCommands() map[string]Command {
-	return map[string]Command{
+func GetCommands() Commands {
+	return Commands{
 		"exit": {
 			Name:        "exit",
 			Description: "Exits the REPL",
