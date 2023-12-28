e326f83f2e18b603a210006644fdc2a0269de17f
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2023-12-27 20:58:44 -0500

    docs: document runrepl function

--- src/repl/runrepl.go
+++ src/repl/runrepl.go
@@ -5,6 +5,10 @@ import (
 	"os"
 )
 
+// Starts a Read-Eval-Print Loop (REPL) for executing commands.
+//
+// It takes a function 'execute' as a parameter, which is responsible for
+// executing the user input.
 func RunREPL(execute func(...string) error) {
 	PrintPrompt()
 
