c8c16fd5ad200072de24dcd99c87556d49295b6d
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2023-12-28 20:48:42 -0500

    test: cover printhelpmessage func

---
+++ src/commands/printhelpmessage_test.go
@@ -0,0 +1,73 @@
+package commands
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"testing"
+)
+
+func TestPrintHelpMessage(t *testing.T) {
+	output := setupTestBed(t)
+	expected := getExpectedOutputFixture()
+
+	if output != expected {
+		t.Errorf("PrintHelpMessage() = %q, want %q", output, expected)
+	}
+}
+
+// @SECTION: Helper Functions
+
+// @TODO: Mock `GetCommands()` to completely isolate this unit test.
+func getExpectedOutputFixture() string {
+	expected := "Welcome to the Pokedex!\nUsage:\n\n"
+
+	for _, command := range GetCommands() {
+		expected += fmt.Sprintf("%s: %s\n", command.Name, command.Description)
+	}
+
+	return expected
+}
+
+func getResponseFromPipe(readPipe, writePipe *os.File) string {
+	if writePipe != nil {
+		writePipe.Close()
+	}
+
+	var buf bytes.Buffer
+	io.Copy(&buf, readPipe)
+
+	return buf.String()
+}
+
+func mockStdout(writePipe *os.File) (savedOriginalStdOut *os.File) {
+	savedOriginalStdOut = os.Stdout
+
+	os.Stdout = writePipe // Override stdout with custom write pipe.
+
+	return savedOriginalStdOut
+}
+
+func performTest(t *testing.T) {
+	if err := PrintHelpMessage(); err != nil {
+		t.Errorf("PrintHelpMessage() returned an error: %v", err)
+	}
+}
+
+func restoreMockedStdout(originalStdout *os.File) {
+	os.Stdout = originalStdout
+}
+
+func setupTestBed(t *testing.T) string {
+	read, write, _ := os.Pipe()
+
+	originalStdout := mockStdout(write)
+	defer restoreMockedStdout(originalStdout)
+
+	performTest(t)
+
+	response := getResponseFromPipe(read, write)
+
+	return response
+}
