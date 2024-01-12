9dd4bff523c4ab610e4f42dc2bf097e9b9adb066
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-01-11 23:45:00 -0500

    refactor(repl): give variables clearer naming

--- src/repl/SanitizeInput.go
+++ src/repl/SanitizeInput.go
@@ -5,9 +5,10 @@ import "strings"
 // This function takes a string and returns a slice of strings after performing
 // the necessary sanitization.
 func SanitizeInput(input string) []string {
-	trimmed := strings.TrimSpace(input)
-	lowered := strings.ToLower(trimmed)
-	words := strings.Fields(lowered)
+	trimmedInput := strings.TrimSpace(input)
+	lowercasedInput := strings.ToLower(trimmedInput)
+
+	words := strings.Fields(lowercasedInput)
 
 	return words
 }
