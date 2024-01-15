c16f285d0db8f80c466b3d8c1598c3a064f48439
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-01-15 01:27:42 -0500

    refactor: add toolchain methods

--- src/commands/toolchain.go
+++ src/commands/toolchain.go
@@ -7,3 +7,19 @@ type IToolchain interface {
 type Toolchain struct {
 	commands *Commands
 }
+
+func (t *Toolchain) AddCommand(command ...Command) {
+	for _, cmd := range command {
+		(*t.commands)[cmd.Name] = cmd
+	}
+}
+
+func (t *Toolchain) DeleteCommand(name string) {
+	delete(*t.commands, name)
+}
+
+func NewToolchain() *Toolchain {
+	return &Toolchain{
+		commands: &Commands{},
+	}
+}
