ad381ca2fbcb9e3b8cf60b3cba059d9b5856ca53
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-01-15 00:30:59 -0500

    feat: add command interface

--- src/commands/command.go
+++ src/commands/command.go
@@ -1,7 +1,18 @@
 package commands
 
+import "fmt"
+
 type Command struct {
 	Name        string
 	Description string
 	Execute     func() error
+}
+
+type ICommand interface {
+	Execute() error
+	PrintCommandHelp()
+}
+
+func (c Command) PrintCommandHelp() {
+	fmt.Printf("%s: %s\n", c.Name, c.Description)
 }
