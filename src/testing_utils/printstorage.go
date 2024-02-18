2f21298632e9ea6ff6427403f3bce3adc43b1aaa
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-02-17 23:42:17 -0500

    test: create util to capture stdout

---
+++ src/testing_utils/printstorage.go
@@ -0,0 +1,39 @@
+package testing_utils
+
+import (
+	"bytes"
+	"io"
+	"os"
+)
+
+type PrintStorage struct {
+	read, write, old *os.File
+}
+
+func (c *PrintStorage) Capture(performTest func()) string {
+	c.captureStdOut()
+
+	performTest()
+
+	c.restoreStdOut()
+
+	return c.getCapturedPrintOut()
+}
+
+func (c *PrintStorage) captureStdOut() {
+	c.old = os.Stdout
+	c.read, c.write, _ = os.Pipe()
+	os.Stdout = c.write
+}
+
+func (c *PrintStorage) getCapturedPrintOut() string {
+	var buf bytes.Buffer
+	io.Copy(&buf, c.read)
+
+	return buf.String()
+}
+
+func (c *PrintStorage) restoreStdOut() {
+	c.write.Close()
+	os.Stdout = c.old
+}
