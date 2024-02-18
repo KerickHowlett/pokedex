0fb51905bba2f2b9b29b702fc43d9f8dc513c0c3
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-02-17 23:43:38 -0500

    refactor: clear out main func body

--- src/main.go
+++ src/main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"github.com/KerickHowlett/pokedexcli/commands"
-	"github.com/KerickHowlett/pokedexcli/repl"
-)
-
 func main() {
-	cmds := commands.Create()
-	commandRunner := func(selection string) error {
-		return commands.Run(selection, cmds)
-	}
 
-	repl.Run(commandRunner)
 }
