dba2d8c2ae1ab8e8166dcf307536d834d10f7507
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-03-16 18:51:55 -0400

    feat(command): create help command factory

--- src/toolchain/printhelpmessage.go
+++ src/toolchain/printhelpmessage.go
@@ -2,6 +2,17 @@ package toolchain
 
 import "fmt"
 
+// PrintHelpMessage prints the help message for the Pokedex toolchain.
+// It prints the welcome message and the help info for all toolchain CLI commands.
+//
+// Example usage:
+//
+//	toolchain := NewToolchain(
+//		WithCommand(c.NewExitCommand()),
+//		WithCommand(c.NewHelpCommand()),
+//	)
+//
+//	toolchain.PrintHelpMessage()
 func (t *Toolchain) PrintHelpMessage() {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println() // Add new line.
