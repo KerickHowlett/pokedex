68066057c86f6ef5a57e60dd49cd2f39c4364ab2
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-06 18:36:24 -0400

    refactor(query): integrate new frozen time fixture for query cache test

--- internal/query/cache/add_test.go
+++ internal/query/cache/add_test.go
@@ -2,35 +2,35 @@ package query_cache
 
 import (
 	"testing"
-	"time"
+
+	f "test_tools/fixtures"
 )
 
 func TestQueryCache_Add(t *testing.T) {
-	runQueryCacheAddTest := func() (queryCache *QueryCache, currentTime time.Time) {
+	runQueryCacheAddTest := func() (queryCache *QueryCache) {
 		queryCache = NewQueryCache()
 
-		currentTime = time.Date(2024, time.April, 6, 12, 0, 0, 0, time.UTC)
-		queryCache.Add(key, []byte(value), currentTime)
+		queryCache.Add(key, []byte(value), f.FrozenTime)
 
-		return queryCache, currentTime
+		return queryCache
 	}
 
 	t.Run("should add new cache entry.", func(t *testing.T) {
-		cache, _ := runQueryCacheAddTest()
+		cache := runQueryCacheAddTest()
 		if _, exists := cache.entry[key]; !exists {
 			t.Errorf("Expected entry with key '%s' to exist in cache, but it doesn't", key)
 		}
 	})
 
 	t.Run("should set createdAt as the current time.", func(t *testing.T) {
-		cache, expectedTime := runQueryCacheAddTest()
-		if entry := cache.entry[key]; !entry.createdAt.Equal(expectedTime) {
-			t.Errorf("Expected createdAt to be %v, but got %v", expectedTime.String(), entry.createdAt.String())
+		cache := runQueryCacheAddTest()
+		if entry := cache.entry[key]; !entry.createdAt.Equal(f.FrozenTime) {
+			t.Errorf("Expected createdAt to be %v, but got %v", f.FrozenTime.String(), entry.createdAt.String())
 		}
 	})
 
 	t.Run("should set value as the provided value with it unmutated.", func(t *testing.T) {
-		cache, _ := runQueryCacheAddTest()
+		cache := runQueryCacheAddTest()
 		if entry := cache.entry[key]; string(entry.value) != value {
 			t.Errorf("Expected value to be '%s', but got '%s'", value, string(entry.value))
 		}
