4c7292248e44553518fbb95e32b100f212775a89
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-05 21:30:23 -0400

    style(main): change conf to config

--- cmd/cli/app/main.go
+++ cmd/cli/app/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	s "shell"
 
-	conf "github.com/KerickHowlett/pokedexcli/cmd/cli/config"
+	"github.com/KerickHowlett/pokedexcli/cmd/cli/config"
 )
 
 func main() {
-	s.PokedexCLIShell(conf.PokedexCLIConfig)
+	s.PokedexCLIShell(config.PokedexCLIConfig)
 }
