d3c713f4d091a4a60d9a8fbd5c408a8f4d9a0fea
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-06 15:36:43 -0400

    style(test_tools): add empty line for spacing

--- internal/test_tools/fixtures/fixtures.go
+++ internal/test_tools/fixtures/fixtures.go
@@ -4,6 +4,7 @@ var (
 	// APIEndpoint represents the URL of the API endpoint domain. which
 	// is used for tests that require an API endpoint.
 	APIEndpoint = "https://example.com"
+
 	// StarterTown represents the name of the starting town in the game,
 	// which is used for filling out Location.Name for tests.
 	StarterTown = "Pallet Town"
