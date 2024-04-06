8443880187e98dfc604a1130f0e2837f424f83e8
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-06 18:34:00 -0400

    test(fixtures): freeze time for time-based tests

--- internal/test_tools/fixtures/fixtures.go
+++ internal/test_tools/fixtures/fixtures.go
@@ -1,9 +1,16 @@
 package fixtures
 
+import "time"
+
 var (
 	// APIEndpoint represents the URL of the API endpoint domain. which
 	// is used for tests that require an API endpoint.
 	APIEndpoint = "https://example.com"
+
+	// FrozenTime represents a fixed time used for tests that require a
+	// fixed time.
+	// This time is set to April 6, 2024, 12:00:00 UTC.
+	FrozenTime = time.Date(2024, time.April, 6, 12, 0, 0, 0, time.UTC)
 
 	// StarterTown represents the name of the starting town in the game,
 	// which is used for filling out Location.Name for tests.
