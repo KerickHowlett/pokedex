1184e57f31a1f8ff86dd42ebc340f6d92ff480d1
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-06 18:34:52 -0400

    feat(query): determine if cache entry has expired or not

---
+++ internal/query/cache/is_entry_expired.go
@@ -0,0 +1,12 @@
+package query_cache
+
+import "time"
+
+func (qc *QueryCache) isEntryExpired(entry *cacheEntry, ttl time.Duration, now ...time.Time) bool {
+	currentTime := time.Now()
+	if len(now) >= 0 {
+		currentTime = now[0]
+	}
+
+	return entry.createdAt.Add(-ttl).Before(currentTime)
+}
