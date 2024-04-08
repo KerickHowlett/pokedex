b455b5edc0984a57ac8de6abea7e7bf452f0e2c5
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-07 23:06:05 -0400

    refactor(command): optimize test suite

--- internal/ui/command/print_help_test.go.go
+++ internal/ui/command/print_help_test.go.go
@@ -9,10 +9,15 @@ import (
 )
 
 func TestPrintHelp(t *testing.T) {
-	printout := utils.NewPrintStorage()
+	setup := func() (command *m.MockCommand, printout *utils.PrintStorage) {
+		command = m.NewMockCommand()
+		printout = utils.NewPrintStorage()
+
+		return command, printout
+	}
 
 	t.Run("should print the command name and description in the proper format.", func(t *testing.T) {
-		command := m.NewMockCommand()
+		command, printout := setup()
 		output := printout.Capture(command.PrintHelp)
 
 		expectedOutput := fmt.Sprintf("%s: %s\n", command.GetName(), command.GetDescription())
