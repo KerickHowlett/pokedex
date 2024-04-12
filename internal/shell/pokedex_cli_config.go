3523e1d3743aee2b38dae3704a1f288d09ed34c5
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-12 19:09:34 -0400

    feat(shell): update shell config schema

--- internal/shell/pokedex_cli_config.go
+++ internal/shell/pokedex_cli_config.go
@@ -3,9 +3,11 @@ package pokedexclishell
 // PokedexCLIConfig represents the configuration for the Pokedex CLI.
 //
 // Fields:
+//   - LocalAreaAPIEndpoint: The API endpoint for the Local Area service.
 //   - Prompt: The text displayed as the command prompt.
 //   - StartingMapsAPIEndpoint: The initial API endpoint for the Maps service.
 type PokedexCLIConfig struct {
+	LocalAreaAPIEndpoint    string
 	Prompt                  string
 	StartingMapsAPIEndpoint string
 }
