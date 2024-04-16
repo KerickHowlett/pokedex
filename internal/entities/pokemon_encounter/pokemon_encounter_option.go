e847640529ef99f0c50834b36775a6f070563f61
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-15 23:25:46 -0400

    refactor(pokemon_encounter): minimize total files for builder options

--- internal/entities/pokemon_encounter/pokemon_encounter_option.go
+++ internal/entities/pokemon_encounter/pokemon_encounter_option.go
@@ -1,3 +1,23 @@
 package pokemon_encounter
 
+import p "pokemon"
+
 type PokemonEncounterOption func(*PokemonEncounter)
+
+// WithPokemon sets the Pokemon of the PokemonEncounter.
+//
+// Parameters:
+//   - pokemon: The Pokemon that can be encountered.
+//
+// Returns:
+//   - PokemonEncounterOption: The option to set the Pokemon of the PokemonEncounter.
+//
+// Example:
+//
+//	pe := &PokemonEncounter{}
+//	WithPokemon(p.Pokemon{Name: "pikachu"})(pe)
+func WithPokemon(pokemon *p.Pokemon) PokemonEncounterOption {
+	return func(pe *PokemonEncounter) {
+		pe.Pokemon = pokemon
+	}
+}
