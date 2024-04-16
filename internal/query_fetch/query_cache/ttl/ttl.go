b7fb9d8b5d4f69502f1adba4fe0e8f347f9dfd1e
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-15 22:34:03 -0400

    feat(query_fetch): add ttl preset to disable cache

--- internal/query_fetch/query_cache/ttl/ttl.go
+++ internal/query_fetch/query_cache/ttl/ttl.go
@@ -4,6 +4,8 @@ import "time"
 
 // TTL (Time-To-Live) presets for cache expiration.
 const (
+	// Disable represents the time-to-live (TTL) duration of zero seconds.
+	Disable = 0 * time.Second
 	// OneDay represents the time-to-live (TTL) duration of one day.
 	OneDay = 24 * time.Hour
 )
