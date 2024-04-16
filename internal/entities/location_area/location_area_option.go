7818cbc230d1d578448142f7bf7742dc6d981615
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-15 23:23:28 -0400

    refactor(location_area): minimize total files for builder options

--- internal/entities/location_area/location_area_option.go
+++ internal/entities/location_area/location_area_option.go
@@ -1,4 +1,30 @@
 package location_area
 
+import pe "pokemon_encounter"
+
 // LocationAreaOption is a function that modifies a LocationArea struct.
 type LocationAreaOption func(*LocationArea)
+
+// WithPokemonEncounter returns a function that sets the
+// Pokemon encounters of a LocationArea.
+//
+// The returned function can be used as a
+// modifier function to set the Pokemon encounters of a
+// LocationArea struct.
+//
+// Parameters:
+//   - encounter: The Pokemon that can be encounter within the LocationArea.
+//
+// Returns:
+//   - A LocationAreaOption function.
+//
+// Example usage:
+//
+//	locationArea := NewLocationArea(
+//		WithPokemonEncounter(pokemon),
+//	)
+func WithPokemonEncounter(encounter pe.PokemonEncounter) LocationAreaOption {
+	return func(la *LocationArea) {
+		la.Encounters = append(la.Encounters, encounter)
+	}
+}
