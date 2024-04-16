c17e15da08eeecc9dc19131db23ae3dfb16e280c
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-16 14:12:03 -0400

    refactor(command): reduce printhelp param requirement

--- internal/ui/command/print_help.go
+++ internal/ui/command/print_help.go
@@ -2,6 +2,16 @@ package command
 
 import "fmt"
 
+// CommandDescription represents an interface for getting a brief description of commands.
+//
+// Methods:
+//   - GetDescription: Returns a brief description of the command.
+//   - GetName: Returns the name of the command.
+type CommandInformation interface {
+	CommandDescription
+	CommandName
+}
+
 // PrintHelp prints the help message for a single command.
 //
 // Parameters:
@@ -9,8 +19,8 @@ import "fmt"
 //
 // Example usage:
 //
-//	command := c.NewExitCommand()
-//	c.PrintHelp(command)
-func PrintHelp(c Command) {
-	fmt.Printf("%s: %s\n", c.GetName(), c.GetDescription())
+//	command := ci.NewExitCommand()
+//	ci.PrintHelp(command)
+func PrintHelp(ci CommandInformation) {
+	fmt.Printf("%s: %s\n", ci.GetName(), ci.GetDescription())
 }
