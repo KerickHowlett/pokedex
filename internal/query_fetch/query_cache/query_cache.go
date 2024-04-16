16da092c98413efac2d09cd7c796ae7e38aecd9f
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-15 23:39:05 -0400

    refactor(query_fetch): minimize total query cache files

--- internal/query_fetch/query_cache/query_cache.go
+++ internal/query_fetch/query_cache/query_cache.go
@@ -1,6 +1,19 @@
 package query_cache
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
+
+// cacheEntry represents a single entry in the cache.
+//
+// Fields:
+//   - createdAt: The time when the cache entry was created.
+//   - value: The cached value.
+type cacheEntry struct {
+	createdAt *time.Time
+	value     []byte
+}
 
 // QueryCache is a cache that stores HTTP query results.
 //
@@ -12,4 +25,104 @@ import "sync"
 type QueryCache struct {
 	entry map[string]cacheEntry
 	mutex *sync.Mutex
+}
+
+// Find retrieves the value associated with the given key from the query cache.
+//
+// Parameters:
+//   - key: The key to retrieve the value for.
+//
+// Returns:
+//   - The value associated with the key, or nil if the key is not found.
+//   - A boolean value indicating whether the key was found in the cache.
+//
+// Example:
+//
+//	value, cacheHit := cache.Find("key")
+//	if !cacheHit {
+//	    log.Println("key not found in cache")
+//	}
+func (qc *QueryCache) Find(key string) (value []byte, entryFound bool) {
+	qc.mutex.Lock()
+	defer qc.mutex.Unlock()
+
+	if entry, entryFound := qc.entry[key]; entryFound {
+		return entry.value, true
+	}
+
+	return nil, false
+}
+
+// Save adds a new entry to the query cache with the specified key and value.
+//
+// The function acquires a lock on the query cache.
+//
+// Parameters:
+//   - key: The key to associate with the value.
+//   - value: The value to be stored in the cache.
+//   - now: The current time to be used as the reference time for the cache. If not provided, the current system time will be used.
+//
+// Example:
+//
+//	 response, err := http.Get("https://example.com/api/v1")
+//		cache.Save("key", response.Body)
+func (qc *QueryCache) Save(key string, value []byte, now ...time.Time) {
+	qc.mutex.Lock()
+	defer qc.mutex.Unlock()
+
+	createdAt := time.Now()
+	if len(now) >= 1 {
+		createdAt = now[0]
+	}
+
+	qc.entry[key] = cacheEntry{
+		createdAt: &createdAt,
+		value:     value,
+	}
+}
+
+// evictionLoop is responsible for evicting expired entries from the query cache.
+//
+// The function acquires a lock on the query cache, iterates over all entries,
+// and deletes any entry that has expired based on the TTL and current time.
+//
+// Parameters:
+//   - ttl: The Time-to-Live (TTL) duration that determines how long each entry will be kept in the cache before being evicted.
+//   - now: The current time to be used as the reference time for the cache. If not provided, the current system time will be used.
+//
+// Example:
+//
+//	 ttl := 5 * time.Minute
+//	 cache := NewQueryCache(ttl)
+//		cache.evictionLoop(ttl)
+func (qc *QueryCache) evictionLoop(ttl time.Duration, now ...time.Time) {
+	qc.mutex.Lock()
+	defer qc.mutex.Unlock()
+
+	for key, entry := range qc.entry {
+		if qc.isEntryExpired(&entry, ttl, now...) {
+			delete(qc.entry, key)
+		}
+	}
+}
+
+// isEntryExpired checks if the cache entry is expired based on the Time-to-Live (TTL) duration.
+//
+// Parameters:
+//   - entry: The cache entry to check for expiration.
+//   - ttl: The Time-to-Live (TTL) duration that determines how long each entry will be kept in the cache before being evicted.
+//   - now: The current time to be used as the reference time for the cache. If not provided, the current system time will be used.
+//
+// Returns:
+//   - A boolean value indicating whether the cache entry is expired.
+//
+// Example:
+//
+//	isExpired := cache.isEntryExpired(entry, 5 * time.Minute)
+func (qc *QueryCache) isEntryExpired(entry *cacheEntry, ttl time.Duration, now ...time.Time) bool {
+	currentTime := time.Now()
+	if len(now) > 0 {
+		currentTime = now[0]
+	}
+	return entry.createdAt.Add(-ttl).Before(currentTime)
 }
--- internal/query_fetch/query_cache/query_cache_test.go
+++ internal/query_fetch/query_cache/query_cache_test.go
@@ -1,6 +1,14 @@
 package query_cache
 
-import "time"
+import (
+	"bytes"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	f "test_tools/fixtures"
+)
 
 const (
 	mockedTTL   = time.Microsecond
@@ -14,4 +22,140 @@ func createCacheEntry(value string, createdAt time.Time) cacheEntry {
 		createdAt: &createdAt,
 		value:     []byte(value),
 	}
+}
+
+func TestQueryCache_Find(t *testing.T) {
+	setup := func() *QueryCache {
+		queryCache := NewQueryCache(24*time.Hour, f.FrozenTime)
+		queryCache.entry[key] = cacheEntry{value: []byte(value)}
+
+		return queryCache
+	}
+
+	t.Run("Cache Hit", func(t *testing.T) {
+		t.Run("should return the value of the cache entry if it exists.", func(t *testing.T) {
+			cache := setup()
+			if cachedValue, _ := cache.Find(key); !bytes.Equal(cachedValue, []byte(value)) {
+				t.Errorf("Expected value to be %v, but got %v", value, cachedValue)
+			}
+		})
+
+		t.Run("should return TRUE if the cache entry exists.", func(t *testing.T) {
+			cache := setup()
+			if _, entryFound := cache.Find(key); !entryFound {
+				t.Error("Expected cache entry to exist, but it does not.")
+			}
+		})
+	})
+
+	t.Run("Cache Miss", func(t *testing.T) {
+		t.Run("should return nil if the cache entry does not exist.", func(t *testing.T) {
+			cache := setup()
+			if cachedValue, _ := cache.Find(invalid_key); cachedValue != nil {
+				t.Errorf("Expected value to be nil, but got %v", cachedValue)
+			}
+		})
+
+		t.Run("should return FALSE if the cache entry does not exist.", func(t *testing.T) {
+			cache := setup()
+			if _, entryFound := cache.Find(invalid_key); entryFound {
+				t.Error("Expected cache entry to not exist, but it does.")
+			}
+		})
+	})
+}
+
+func TestQueryCache_Save(t *testing.T) {
+	runQueryCacheSaveTest := func() (queryCache *QueryCache) {
+		queryCache = NewQueryCache(mockedTTL)
+
+		queryCache.Save(key, []byte(value), f.FrozenTime)
+
+		return queryCache
+	}
+
+	t.Run("should save new cache entry.", func(t *testing.T) {
+		cache := runQueryCacheSaveTest()
+		if _, exists := cache.entry[key]; !exists {
+			t.Errorf("Expected entry with key '%s' to exist in cache, but it doesn't", key)
+		}
+	})
+
+	t.Run("should set createdAt as the current time.", func(t *testing.T) {
+		cache := runQueryCacheSaveTest()
+		if entry := cache.entry[key]; !entry.createdAt.Equal(f.FrozenTime) {
+			t.Errorf("Expected createdAt to be %v, but got %v", f.FrozenTime.String(), entry.createdAt.String())
+		}
+	})
+
+	t.Run("should set value as the provided value with it unmutated.", func(t *testing.T) {
+		cache := runQueryCacheSaveTest()
+		if entry := cache.entry[key]; string(entry.value) != value {
+			t.Errorf("Expected value to be '%s', but got '%s'", value, string(entry.value))
+		}
+	})
+}
+
+func TestQueryCache_evictionLoop(t *testing.T) {
+	setup := func() (queryCache *QueryCache) {
+		queryCache = NewQueryCache(mockedTTL)
+
+		queryCache.entry["key1"] = createCacheEntry("value_1", f.FrozenTime.Add(-time.Hour))
+		queryCache.entry["key2"] = createCacheEntry("value_2", f.FrozenTime.Add(-time.Minute))
+		queryCache.entry["key3"] = createCacheEntry("value_3", f.FrozenTime.Add(time.Hour))
+
+		return queryCache
+	}
+
+	expectCacheEntryExistence := func(key string, exists bool, cache *QueryCache) {
+		existsLabel := strings.ToUpper(strconv.FormatBool(exists))
+		if _, found := cache.entry[key]; found != exists {
+			t.Errorf("Expected key '%s' to exist in the cache: %s, but it is not", key, existsLabel)
+		}
+	}
+
+	expectEntryCount := func(length int, cache *QueryCache) {
+		if entryCount := len(cache.entry); entryCount != length {
+			t.Errorf("Expected cache to have %d entries, but only found %d instead", length, entryCount)
+		}
+	}
+
+	t.Run("should only remove the expired cache entries.", func(t *testing.T) {
+		cache := setup()
+
+		// Preliminary Check to weed out possible false positives.
+		expectEntryCount(3, cache)
+		cache.evictionLoop(time.Minute, f.FrozenTime)
+
+		expectEntryCount(1, cache)
+		expectCacheEntryExistence("key1", false, cache)
+		expectCacheEntryExistence("key2", false, cache)
+		expectCacheEntryExistence("key3", true, cache)
+	})
+}
+
+func TestIsEntryExpired(t *testing.T) {
+	setup := func(createdAt time.Time) (queryCache *QueryCache, entry *cacheEntry, ttl time.Duration) {
+		queryCache = &QueryCache{}
+		entry = &cacheEntry{
+			createdAt: &createdAt,
+		}
+		ttl = time.Hour
+
+		return queryCache, entry, ttl
+	}
+
+	t.Run("should return TRUE if entry has expired.", func(t *testing.T) {
+		cache, entry, ttl := setup(f.FrozenTime)
+		if expired := cache.isEntryExpired(entry, ttl, f.FrozenTime); !expired {
+			t.Errorf("Expected entry to be expired, but it is not")
+		}
+	})
+
+	t.Run("should return FALSE if entry has NOT expired.", func(t *testing.T) {
+		cache, entry, ttl := setup(f.FrozenTime.Add(time.Hour * 24))
+		if expired := cache.isEntryExpired(entry, ttl, f.FrozenTime); expired {
+			t.Errorf("Expected entry to NOT be expired, but it is")
+		}
+	})
 }
