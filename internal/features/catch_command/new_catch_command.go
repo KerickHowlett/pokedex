04c8cc3e085fdda23eb6cee402dae8fdb0c7fa45
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-17 14:41:03 -0400

    refactor(catch_command): improve catch rate

--- internal/features/catch_command/new_catch_command.go
+++ internal/features/catch_command/new_catch_command.go
@@ -9,12 +9,23 @@ import (
 	"query_fetch/query_cache/ttl"
 )
 
+// NewCatchCommand returns a new instance of the CatchCommand struct.
+//
+// Parameters:
+//   - options: The options to set on the CatchCommand.
+//
+// Returns:
+//   - A new instance of the CatchCommand struct.
+//
+// Example usage:
+//
+//	command := NewCatchCommand()
 func NewCatchCommand(options ...CatchCommandOption) *CatchCommand {
 	command := &CatchCommand{
 		battleOpponent:              p.NewPokemon(),
 		cacheTTL:                    ttl.OneDay,
 		catchPokemon:                query_fetch.QueryFetch[p.Pokemon],
-		checkYourLuck:               func() int { return rand.Int() },
+		checkYourLuck:               defaultCheckYourLuck,
 		description:                 "Catch That Pokemon! Usage: catch <pokemon-name>",
 		escapedNotification:         "escaped!",
 		name:                        "catch",
@@ -33,4 +44,20 @@ func NewCatchCommand(options ...CatchCommandOption) *CatchCommand {
 	}
 
 	return command
+}
+
+// defaultCheckYourLuck is the default implementation of the CheckYourLuck function.
+//
+// Parameters:
+//   - pokemonBaseExperience: The base experience of the pokemon to catch.
+//
+// Returns:
+//   - The chance of catching the Pokemon.
+//
+// Example usage:
+//
+//	chance := defaultCheckYourLuck(100)
+func defaultCheckYourLuck(pokemonBaseExperience int) (chance int) {
+	chance = rand.Intn(100)
+	return chance
 }
